e100738c9ea0564496c2418e5408a3de4b903f71
trysingtime@gmail.com <trysingtime@gmail.com>
2021-08-16 15:12:36 +0800

    keyboard Shift/CapsLock/NumLock/ScrollLock

--- source/bootpack.c
+++ source/bootpack.c
@@ -9,30 +9,34 @@ void putfonts8_asc_layer(struct LAYER *layer, int x, int y, int color, int backc
 void task_b_implement(struct LAYER *layer_back);
 void task_console_implement(struct LAYER *layer_back);
 
+#define KEYCMD_LED      0xed
+
 void HariMain(void) {
     struct BOOTINFO *bootinfo = (struct BOOTINFO *)0x0ff0; // 获取asmhead.nas中存入的bootinfo信息
-    struct FIFO32 fifo;
-    int fifobuf[128];
     char s[40];
-    struct TIMER *timer;
-    int mx, my, i, cursor_x, cursor_c;
-    int currentwindow = 0;
-    unsigned int memorytotal;
-    struct MOUSE_DEC mdec;
-    struct MEMMNG *mng = (struct MEMMNG *) MEMMNG_ADDR;
-    struct LAYERCTL *layerctl;
-    struct LAYER *layer_back, *layer_mouse, *layer_window, *layer_window_b[3], *layer_console;
-    unsigned char *buf_back, buf_mouse[256], *buf_window, *buf_window_b, *buf_console;
+
     /* 键盘按键映射表 */
-    static char keytable[0x54] = {
+    static char keytable0[0x80] = {
 		0,   0,   '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 0,   0,
 		'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '[', ']', 0,   0,   'A', 'S',
-		'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', '\'', '`',   0,   '\\', 'Z', 'X', 'C', 'V',
+		'D', 'F', 'G', 'H', 'J', 'K', 'L', ';', '\'', '`',0,   '\\','Z', 'X', 'C', 'V',
 		'B', 'N', 'M', ',', '.', '/', 0,   '*', 0,   ' ', 0,   0,   0,   0,   0,   0,
 		0,   0,   0,   0,   0,   0,   0,   '7', '8', '9', '-', '4', '5', '6', '+', '1',
-		'2', '3', '0', '.'
+		'2', '3', '0', '.', 0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+		0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+		0,   0,   0,   '-', 0,  0,   0,   0,   0,   0,   0,   0,   0,   '\\', 0,  0
 	};
-    struct TASK *task_a, *task_b[3], *task_console;
+    static char keytable1[0x80] = {
+		0,   0,   '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', 0,   0,
+		'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', 0,   0,   'A', 'S',
+		'D', 'F', 'G', 'H', 'J', 'K', 'L', ':', '"', '~',  0,   '|', 'Z', 'X', 'C', 'V',
+		'B', 'N', 'M', '<', '>', '?', 0,   '*', 0,   ' ', 0,   0,   0,   0,   0,   0,
+		0,   0,   0,   0,   0,   0,   0,   '7', '8', '9', '-', '4', '5', '6', '+', '1',
+		'2', '3', '0', '.', 0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+		0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,   0,
+		0,   0,   0,   '_', 0,   0,   0,   0,   0,   0,   0,   0,   0,   '|', 0,   0
+	};
+    int currentwindow = 0, key_shift = 0, key_leds = (bootinfo->leds >> 4) & 7;
 
 // 设置系统参数
     init_gdtidt(); // 初始化GDT/IDT
@@ -49,21 +53,41 @@ void HariMain(void) {
     256~511     键盘输入(键盘控制器读入的值再加上256)
     512~767     鼠标输入(键盘控制器读入的值再加上512)
 */
-    fifo32_init(&fifo, 128, fifobuf, 0); // 初始化通用(键盘/鼠标/倒计时)缓冲区
+    // 初始化通用(键盘/鼠标/倒计时)缓冲区
+    struct FIFO32 fifo;
+    int fifobuf[128];
+    fifo32_init(&fifo, 128, fifobuf, 0);
 
 // 启用键盘/PIC1/鼠标(IRQ1, IRQ2, IRQ12)
 	io_out8(PIC0_IMR, 0xf9); /* 开放键盘和PIC1中断(11111001) */
 	io_out8(PIC1_IMR, 0xef); /* 开放鼠标中断(11101111) */
     init_keyboard(&fifo, 256); // 初始化键盘控制电路(包含鼠标控制电路)
+    struct MOUSE_DEC mdec;
     enable_mouse(&fifo, 512, &mdec); // 启用鼠标本身
+
+// 初始化键盘锁定键LED状态(CapsLock, NumLock, ScrollLock)
+/*
+    要控制键盘锁定键LED的状态, 需要向键盘发送EDXX数据
+    其中XX的bit0代表ScrollLock, bit1代表NumLock, bit2代表CapsLock(0表示熄灭, 1表示点亮)
+    这里先将EDXX放到缓冲区, 后续由缓冲区触发, 需先发送ED, 再发送XX, 共发送两次
+*/
+    struct FIFO32 keycmd;
+    int keycmd_buf[32];
+    int keycmd_wait = -1; // 用于控制是否需要发送数据到键盘(可以控制是否需要重发), -1代表可以发送下一数据
+    fifo32_init(&keycmd, 32, keycmd_buf, 0);
+    fifo32_put(&keycmd, KEYCMD_LED);
+    fifo32_put(&keycmd, key_leds);
 
 // 启用定时器(IRQ0)
     io_out8(PIC0_IMR, 0xf8); /* 开放定时器中断(11111000)*/
+    struct TIMER *timer;
     timer = timer_alloc();
     timer_init(timer, &fifo, 1);
     timer_settime(timer, 50); // 0.5s
 
-// 管理内存    
+// 管理内存
+    struct MEMMNG *mng = (struct MEMMNG *) MEMMNG_ADDR;
+    unsigned int memorytotal;
     memorytotal = memtest(0x00400000, 0xbfffffff); // 获取总内存大小
     memmng_init(mng);
     memory_free(mng, 0x00001000, 0x0009e000); // 0x00001000~0x0009e000暂未使用, 释放掉
@@ -73,6 +97,9 @@ void HariMain(void) {
     init_palette(); // 设定调色盘
 
 // 管理图层
+    struct LAYERCTL *layerctl;
+    struct LAYER *layer_back, *layer_mouse, *layer_window, *layer_window_b[3], *layer_console;
+    unsigned char *buf_back, buf_mouse[256], *buf_window, *buf_window_b, *buf_console;
     layerctl = layerctl_init(mng, bootinfo->vram, bootinfo->screenx, bootinfo->screeny); // 初始化图层管理
     // 背景图层
     layer_back = layer_alloc(layerctl); // 新建背景图层
@@ -80,6 +107,7 @@ void HariMain(void) {
     layer_init(layer_back, buf_back, bootinfo->screenx, bootinfo->screeny, -1); // 初始化背景图层
     init_screen8(buf_back, bootinfo -> screenx, bootinfo -> screeny); // 绘制背景
     // 绘制鼠标坐标(背景图层)
+    int mx, my;
     mx = (bootinfo -> screenx - 16) / 2; // 计算屏幕中间点(减去指针本身)
     my = (bootinfo -> screeny - 28 - 16) / 2; // 计算屏幕中间点(减去任务栏和指针本身)
     sprintf(s, "(%3d, %3d)", mx, my);
@@ -103,11 +131,13 @@ void HariMain(void) {
     make_window8(buf_window, 144, 52, "task_a", 1); // 绘制窗口
     // 绘制窗口图层-文本框
     make_textbox8(layer_window, 8, 28, 144, 16, COL8_FFFFFF);
+    int cursor_x, cursor_c;
     cursor_x = 8; // 光标位置
     cursor_c = COL8_FFFFFF; // 光标颜色
 
 // 多任务
     // 任务控制器初始化, 并返回当前任务
+    struct TASK *task_a, *task_b[3], *task_console;
     task_a = taskctl_init(mng);
 
     // 主任务
@@ -115,6 +145,7 @@ void HariMain(void) {
     fifo.task = task_a; // 若task_a休眠时有中断, 则唤醒task_a
 
     // 任务B(多窗口任务)
+    int i;
     for (i = 0; i < 3; i++) {
         // 窗口图层
         layer_window_b[i] = layer_alloc(layerctl); // 新建窗口图层
@@ -184,6 +215,15 @@ void HariMain(void) {
 
 // 键盘和鼠标输入处理
     for (;;) {
+        /* 键盘LEDS控制缓冲区处理 */
+        if (fifo32_status(&keycmd) > 0 && keycmd_wait < 0) {
+            // 如果键盘缓冲区有数据, 则发送
+            keycmd_wait = fifo32_get(&keycmd);
+            wait_KBC_sendready();
+            io_out8(PORT_KEYDAT, keycmd_wait);
+        }
+
+        /* 中断缓冲区处理 */
         io_cli();
         if (fifo32_status(&fifo) == 0) {
             // 缓冲区为空
@@ -196,57 +236,121 @@ void HariMain(void) {
             io_sti();
             if (256 <= i && i <= 511) {
                 // 键盘缓冲区处理
-                // 显示键盘数据
+                /* 显示键盘数据 */
                 sprintf(s, "%02X", i - 256);
                 putfonts8_asc_layer(layer_back, 0, 16, COL8_FFFFFF, COL8_008484, s, 2);
-                if (i < 256 + 0x54) {
-                    // 键盘数据含于键盘按键表中
-                    if (keytable[i - 256] != 0 && keytable[i - 256] != 0) {
-                        /* 普通字符 */
-                        if (currentwindow == 0) {
-                            // 当前任务窗口, 直接显示
-                            if (cursor_x < 144) {
-                                s[0] = keytable[i - 256];
-                                s[1] = 0;
-                                putfonts8_asc_layer(layer_window, cursor_x, 28, COL8_000000, COL8_FFFFFF, s, 1); // 显示键盘按键
-                                cursor_x += 8; // 光标前移
-                            }
-                        } else {
-                            // 不同任务窗口, 发送到该任务绑定的中断缓冲区
-                            fifo32_put(&task_console->fifo, keytable[i - 256] + 256); // 发送对应键盘字符的ASCII
-                        }
+                /* 将按键编码转换为字符编码 */
+                if (i < 256 + 0x80) {
+                    if (key_shift == 0) {
+                        s[0] = keytable0[i - 256];
+                    } else {
+                        s[0] = keytable1[i - 256];
                     }
-                    if (i == 256 + 0x0e && cursor_x > 8) {
-                        /* 退格键 */
-                        if (currentwindow == 0) {
-                            // 当前任务窗口, 直接显示
-                            if (cursor_x > 8) {
-                                putfonts8_asc_layer(layer_window, cursor_x, 28, COL8_000000, COL8_FFFFFF, " ", 1); // 擦除显示的键盘按键
-                                cursor_x -= 8; // 光标后移
-                            }
-                        } else {
-                            // 不同任务窗口, 发送到该任务绑定的中断缓冲区
-                            fifo32_put(&task_console->fifo, 8 + 256); // 发送对应键盘字符的ASCII, ASCII中空格为8
-                        }
-                    }
-                    if (i == 256 + 0x0f) {
-                        /* TAB键 */
-                        if (currentwindow == 0) {
-                            currentwindow = 1;
-                            make_title8(buf_window, layer_window->bxsize, "task_a", 0);
-                            make_title8(buf_console, layer_console->bxsize, "console", 1);
-                        } else {
-                            currentwindow = 0;
-                            make_title8(buf_window, layer_window->bxsize, "task_a", 1);
-                            make_title8(buf_console, layer_console->bxsize, "console", 0);                            
-                        }
-                        layer_refresh(layer_window, 0 ,0, layer_window->bxsize, 21);
-                        layer_refresh(layer_console, 0 ,0, layer_console->bxsize, 21);
-                    }
-                    // 重绘光标
-                    boxfill8(layer_window->buf, layer_window->bxsize, cursor_c, cursor_x, 28, cursor_x + 7, 43); // 显示白色
-                    layer_refresh(layer_window, cursor_x, 28, cursor_x + 8, 44); // 刷新图层
+                } else {
+                    s[0] = 0;
                 }
+                /* 大小写转换 */
+                if ('A' <= s[0] && s[0] <= 'Z') {
+                    // BOOTINFO->LEDS的bit6存储CapsLock(大小写锁定)状态(bit4:ScrollLock, bit5: NumLock), key_leds右移了4位, 因此现在存于bit2
+                    if (((key_leds & 4) == 0 && key_shift == 0) || ((key_leds & 4) != 0 && key_shift != 0)) {
+                        // CapsLock和Shift相同时, 转换为小写
+                        s[0] += 0x20;
+                    }
+                }
+                /* 不同字符编码处理 */
+                if (s[0] != 0) {
+                    /* 普通字符 */
+                    if (currentwindow == 0) {
+                        // 当前任务窗口, 直接显示
+                        if (cursor_x < 128) {
+                            s[1] = 0;
+                            putfonts8_asc_layer(layer_window, cursor_x, 28, COL8_000000, COL8_FFFFFF, s, 1); // 显示键盘按键
+                            cursor_x += 8; // 光标前移
+                        }
+                    } else {
+                        // 不同任务窗口, 发送到该任务绑定的中断缓冲区
+                        fifo32_put(&task_console->fifo, s[0] + 256); // 发送对应键盘字符的ASCII
+                    }
+                }
+                if (i == 256 + 0x0e && cursor_x > 8) {
+                    /* 退格键 */
+                    if (currentwindow == 0) {
+                        // 当前任务窗口, 直接显示
+                        if (cursor_x > 8) {
+                            putfonts8_asc_layer(layer_window, cursor_x, 28, COL8_000000, COL8_FFFFFF, " ", 1); // 擦除显示的键盘按键
+                            cursor_x -= 8; // 光标后移
+                        }
+                    } else {
+                        // 不同任务窗口, 发送到该任务绑定的中断缓冲区
+                        fifo32_put(&task_console->fifo, 8 + 256); // 发送对应键盘字符的ASCII, ASCII中空格为8
+                    }
+                }
+                if (i == 256 + 0x0f) {
+                    /* TAB键 */
+                    if (currentwindow == 0) {
+                        currentwindow = 1;
+                        make_title8(buf_window, layer_window->bxsize, "task_a", 0);
+                        make_title8(buf_console, layer_console->bxsize, "console", 1);
+                    } else {
+                        currentwindow = 0;
+                        make_title8(buf_window, layer_window->bxsize, "task_a", 1);
+                        make_title8(buf_console, layer_console->bxsize, "console", 0);                            
+                    }
+                    layer_refresh(layer_window, 0 ,0, layer_window->bxsize, 21);
+                    layer_refresh(layer_console, 0 ,0, layer_console->bxsize, 21);
+                }
+                /* Shitf键处理: 左Shift按下置1, 右Shift按下置2, 两个按下置3, 两个都不按置0 */
+                if (i == 256 + 0x2a) {
+                    /* 左Shift按下 */
+                    key_shift |= 1;
+                }
+                if (i == 256 + 0x36) {
+                    /* 右Shift按下 */
+                    key_shift |= 2;
+                }
+                if (i == 256 + 0xaa) {
+                    /* 左Shift抬起 */
+                    key_shift &= ~1;
+                }
+                if (i == 256 + 0xb6) {
+                    /* 右Shift抬起 */
+                    key_shift &= ~2;
+                }
+                /* 各种锁定键处理key_leds(bit2: CapsLock, bit1: NumLock, bit0:ScrollLock) */
+                if (i == 256 + 0x3a) {
+                    /* CapsLock */
+                    key_leds ^= 4;
+                    // 向键盘发送EDXX, 控制LED状态
+                    fifo32_put(&keycmd, KEYCMD_LED);
+                    fifo32_put(&keycmd, key_leds);
+                }
+                if (i == 256 + 0x45) {
+                    /* NUMLock */
+                    key_leds ^= 2;
+                    // 向键盘发送EDXX, 控制LED状态
+                    fifo32_put(&keycmd, KEYCMD_LED);
+                    fifo32_put(&keycmd, key_leds);                    
+                }
+                if (i == 256 + 0x46) {
+                    /* ScrollLock */
+                    key_leds ^= 1;
+                    // 向键盘发送EDXX, 控制LED状态
+                    fifo32_put(&keycmd, KEYCMD_LED);
+                    fifo32_put(&keycmd, key_leds);                    
+                }
+                if (i == 256 + 0xfa) {
+                    /* 键盘接收数据成功 */
+                    keycmd_wait = -1; // 可以发送下一数据
+                }
+                if (i == 256 + 0xfe) {
+                    /* 键盘接收数据失败, 再次发送 */
+                    wait_KBC_sendready();
+                    io_out8(PORT_KEYDAT, keycmd_wait);
+                }
+
+                // 重绘光标
+                boxfill8(layer_window->buf, layer_window->bxsize, cursor_c, cursor_x, 28, cursor_x + 7, 43); // 显示白色
+                layer_refresh(layer_window, cursor_x, 28, cursor_x + 8, 44); // 刷新图层
             } else if (512 <= i && i <= 767) {
                 // 鼠标缓冲区处理
                 if (mouse_decode(&mdec, i - 512) == 1) {
